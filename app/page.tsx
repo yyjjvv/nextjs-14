ecfb3ce4d09980e1a3e96cad04e193aa5bac4360
yyjjvv <jiwonyoo66@gmail.com>
2024-04-12 23:40:17 +0900

    about layout nesting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../components/navigation";
-
 export default function app() {
     return (
         <div>
-            <Navigation />
             <h1>Hello!</h1>
         </div>
     );
