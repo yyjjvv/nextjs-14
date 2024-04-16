e3499fb3795d43f24bbca4c5cf9eea9ea335e583
yyjjvv <jiwonyoo66@gmail.com>
2024-04-16 23:37:26 +0900

    fetch - client side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,28 @@
 // () doesm't effect to URL. It just the group
+"use client"
+import { useEffect, useState } from "react";
 
-export const metadata = {
-    title: "Home",
-};
+// export const metadata = {
+//     title: "Home",
+// };
 
-export default function app() {
-    return (
-        <div>
-            <h1>Hello!</h1>
-        </div>
-    );
+export default function Page() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = async () => {
+        const response = await fetch(
+            "https://nomad-movies.nomadcoders.workers.dev/movies"
+        );
+        const json = await response.json();
+        console.log(json)
+        setMovies(json);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
