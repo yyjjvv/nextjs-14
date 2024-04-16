332e3c4864200418c9c76258e55f379af4004ac3
yyjjvv <jiwonyoo66@gmail.com>
2024-04-17 00:08:03 +0900

    add loading component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,13 +6,13 @@ export const metadata = {
 
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 const getMovies = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
     const response = await fetch(URL); //자동으로 페치된 데이터 캐싱
     const json = await response.json();
     return json;
 };
 
-export default async function HomePage() {
+export default async function HomePage() { //async 사용 이유: 로딩이 먼저 이루어져야하기 때문
     const movies = await getMovies();
 
     return <div>{JSON.stringify(movies)}</div>;
