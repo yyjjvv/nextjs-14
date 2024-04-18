b04fce5ad7013ed1f4d3831d37ceba88943d0f50
yyjjvv <jiwonyoo66@gmail.com>
2024-04-18 23:52:06 +0900

    parallel requests

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,30 @@
 // () doesm't effect to URL. It just the group
 
+import Link from "next/link";
+
 export const metadata = {
     title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 const getMovies = async () => {
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(URL); //자동으로 페치된 데이터 캐싱
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(API_URL); //자동으로 페치된 데이터 캐싱
     const json = await response.json();
     return json;
 };
 
-export default async function HomePage() { //async 사용 이유: 로딩이 먼저 이루어져야하기 때문
+export default async function HomePage() {
+    //async 사용 이유: 로딩이 먼저 이루어져야하기 때문
     const movies = await getMovies();
 
-    return <div>{JSON.stringify(movies)}</div>;
+    return (
+        <ul>
+            {movies.map((movie) => (
+                <li key={movie.id}>
+                    <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+            ))}
+        </ul>
+    );
 }
---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+    return <h1>Loading a movie id..</h1>;
+}
