cb303d0f34b3d63140fe6d443f519beb2fd91c99
yyjjvv <jiwonyoo66@gmail.com>
2024-04-25 00:44:05 +0900

    add movieDetail css

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -26,7 +26,6 @@ export default async function MovieDetail({
     // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
     return (
         <div>
-            <h2>Movie Detail</h2>
             <Suspense fallback={<h1>Loading movie info..</h1>}>
                 <MovieInfo id={id} />
             </Suspense>
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie_video.module.css";
 
 async function getVideos(id: string) {
     console.log(`Fetching videos: ${Date.now()}`);
@@ -9,5 +10,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
     const videos = await getVideos(id);
-    return <h1>{JSON.stringify(videos)}</h1>;
+    return (
+        <div className={styles.container}>
+            {videos.map((video) => (
+                <iframe
+                    key={video.id}
+                    src={`https://youtube.com/embed/${video.key}`}
+                    title={video.name}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+                    allowFullScreen
+                />
+            ))}
+        </div>
+    );
 }
