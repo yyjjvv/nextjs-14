30f3a1a1ea54fa44033b7e79d27d8c13dbbff559
yyjjvv <jiwonyoo66@gmail.com>
2024-04-24 23:56:55 +0900

    add navigation.module.css

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,15 +3,14 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
     const path = usePathname();
-    const [count, setCount] = useState(0);
 
     return (
-        <nav>
-            <ul>
+        <nav className={styles.nav}>
+            <ul className={styles.lists}>
                 <li>
                     <Link href="/">Home</Link> {path === "/" ? "❤️" : ""}
                 </li>
